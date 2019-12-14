d660ff3ad02d41191cb1c93d63ee42f8a833bc37
derushio <derushio@gmail.com>
2019-12-14 16:12:29 +0900

    <master>[modified] とりあえず

--- src/main.py
+++ src/main.py
@@ -1,4 +1,12 @@
 import cv2;
 
-cap = cv2.VideoCapture('../data/video.mp4')
-print(cap)
+cap = cv2.VideoCapture('./data/video.mp4')
+ret, frame = cap.read()
+print(frame)
+
+# while(cap.isOpened()):
+    # cv2.imshow('frame', frame)
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+        # break
+
+cap.release()
