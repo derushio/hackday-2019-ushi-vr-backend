8211c44607995a42bdf6156b326e56574e6b0045
derushio <derushio@gmail.com>
2019-12-14 19:23:04 +0900

    <master>[modified] temp

--- src/convert_cow.py
+++ src/convert_cow.py
@@ -2,14 +2,16 @@ import cv2
 import numpy as np
 from video_loader import video_loader
 from statics import video_size
+import copy
 
 def convert_cow(path: str):
     sample_div = 2
 
     frames = video_loader(path);
     new_frames = []
-    for frame in frames:
+    for i_frame, frame in enumerate(frames):
         new_frame = []
+
         for x in frame:
             new_x = []
             for i, y in enumerate(x):
@@ -18,15 +20,26 @@ def convert_cow(path: str):
 
             new_frame.append(new_x)
 
-        for x in new_frame:
-            x_len = len(x) - 1
-            for y in x[0:x_len]:
-                x.append(y)
+        main_frame = copy.deepcopy(new_frame)
 
-        print(np.array(new_frame).shape)
-        cv2.imshow('new_frame', np.array(new_frame))
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        for xi, x in enumerate(main_frame):
+            x_len = (len(x) - 1) // sample_div
+            for y in x[:x_len]:
+                new_frame[xi].append(y)
+
+        for xi, x in enumerate(main_frame):
+            x_len = (len(x) - 1) // sample_div
+            for y in reversed(x[x_len:]):
+                new_frame[xi].insert(0, y)
 
         new_frames.append(new_frame)
-        break;
+
+        print('done: ' + str(i_frame))
+
+    fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
+    video = cv2.VideoWriter('video.mp4', fourcc, 20.0, (640, 480))
+
+    for frame in new_frames:
+        video.write(frame)
+
+    video.release()
